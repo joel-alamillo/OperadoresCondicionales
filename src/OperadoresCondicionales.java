8603b70b75e7deaf6d011f5fe1fd4c7d60e8076c
Joel Alamillo <alamillojuarez@gmail.com>
2024-04-02 21:04:04 -0600

    Version 1. Uso de condicional if else con uso de and y or.

--- src/OperadoresCondicionales.java
+++ src/OperadoresCondicionales.java
@@ -1,8 +1,30 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class OperadoresCondicionales {
     public static void main(String[] args) {
+        //operaciones condicionales and or
+        var a = 11;
+        var valorMinimo = 0;
+        var valorMaximo = 10;
 
+        //comprobacion si esta entre el rango 0 y 10
+        var resultado = a >= valorMinimo && a <= valorMaximo;
+
+        if (resultado){
+            System.out.println("Esta dentro del rango 1 al 10");
+        }else{
+            System.out.println("Esta fuera del rango");
         }
+
+        var vacaciones = true;
+        var diaDescanso = false;
+
+        if(vacaciones || diaDescanso) {
+            System.out.println("Padre puede asistir al juego de su hijo");
+        }else{
+            System.out.println("El padre esta ocupado");
+        }
+
+
     }
 }
